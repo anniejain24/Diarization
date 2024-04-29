7dd19098fad986436c4da402dc0340976a42ae7f
Dhvani Jain <dhvanijain@gmail.com>
2024-04-29 13:28:04 -0500

    working pipeline for gpt3.5 timestamp addition

--- add-timestamps/diarize-levenshtein-timestamps/read_transcript.py
+++ add-timestamps/diarize-levenshtein-timestamps/read_transcript.py
@@ -3,7 +3,7 @@ from promptflow import tool
 import json
 
 @tool
-def read_transcript_from_id(input1: str) -> str:
+def read_transcript_from_id(input1: str) -> dict:
 
     input1 = input1.split('.')[0]
     path_to_data_folder = '/archive/shared/sim_center/shared/ameer/'
@@ -22,39 +22,6 @@ def read_transcript_from_id(input1: str) -> str:
     # a dictionary
     json_transcript = json.load(f)
 
-    #debugging line, change as needed:
-    #json_transcript = json_transcript[:8*int(len(json_transcript)/9)]
 
-    # uncomment as necessary for segmenting the transcript in thirds:
-
-    #json_transcript = json_transcript[:int(len(json_transcript)/2)]
-    #json_transcript = json_transcript[int(len(json_transcript)/3):2*int(len(json_transcript)/3)]
-    #json_transcript = json_transcript[2*int(len(json_transcript)/3):]
-
-    # segmenting the transcript in sixths:
-
-    #json_transcript = json_transcript[:int(len(json_transcript)/6)]
-    #json_transcript = json_transcript[int(len(json_transcript)/6):2*int(len(json_transcript)/6)]
-    #json_transcript = json_transcript[2*int(len(json_transcript)/6):3*int(len(json_transcript)/6)]
-    #json_transcript = json_transcript[3*int(len(json_transcript)/6):4*int(len(json_transcript)/6)]
-    #json_transcript = json_transcript[4*int(len(json_transcript)/6):5*int(len(json_transcript)/6)]
-    #json_transcript = json_transcript[5*int(len(json_transcript)/6):]
-
-    # segmenting the transcript in ninths:
-
-    #json_transcript = json_transcript[:int(len(json_transcript)/9)]
-    #json_transcript = json_transcript[int(len(json_transcript)/9):2*int(len(json_transcript)/9)]
-    #json_transcript = json_transcript[2*int(len(json_transcript)/9):3*int(len(json_transcript)/9)]
-    #json_transcript = json_transcript[3*int(len(json_transcript)/9):4*int(len(json_transcript)/9)]
-    #json_transcript = json_transcript[4*int(len(json_transcript)/9):5*int(len(json_transcript)/9)]
-    #json_transcript = json_transcript[5*int(len(json_transcript)/9):6*int(len(json_transcript)/9)]
-    #json_transcript = json_transcript[6*int(len(json_transcript)/9):7*int(len(json_transcript)/9)]  
-    #json_transcript = json_transcript[7*int(len(json_transcript)/9):8*int(len(json_transcript)/9)] 
-    #json_transcript = json_transcript[8*int(len(json_transcript)/9):]  
-
-    transcript = ''
-    for line in json_transcript:
-        transcript += line['text']
-
-    return transcript
+    return json_transcript
     # return input1
