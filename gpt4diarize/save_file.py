e43afd6c9fe45be0bd465f3f6c3d29f2e59b03c4
Dhvani Jain <dhvanijain@gmail.com>
2024-05-14 12:00:43 -0500

    diarization evaluation

---
+++ gpt4diarize/save_file.py
@@ -0,0 +1,21 @@
+
+from promptflow import tool
+import json
+
+
+# The inputs section will change based on the arguments of the tool function, after you save the code
+# Adding type to arguments and return value will help the system show the types properly
+# Please update the function name/signature per need
+@tool
+def save_file(input1: str, input2: str) -> str:
+    
+    path = '/archive/shared/sim_center/shared/annie/gpt4-3chunk-new/'
+    with open(path + input2.split('.')[0] + ".json", "w") as outfile:
+        json.dump(input1, outfile)
+    
+    return input1
+        
+        
+
+
+    
