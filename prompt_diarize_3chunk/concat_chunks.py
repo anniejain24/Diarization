0b2fce686756d70e066764f7283bc8af8ac9e892
Dhvani Jain <dhvanijain@gmail.com>
2024-06-05 15:53:57 -0500

    promptflow and evaluation edits

--- prompt_diarize_3chunk/concat_chunks.py
+++ prompt_diarize_3chunk/concat_chunks.py
@@ -6,9 +6,11 @@ import json
 def concatenate(input1: str, input2: str, input3: str) -> str:
     transcript = input1 + '\n\n' + input2 + '\n\n' + input3
 
+    '''
     out = ''
     for line in transcript.split('\n\n'):
         if line == '\n': continue
-        out += line + ' (??, ??)' + '\n' 
+        out += line + '\n'
+    '''
     
-    return out
+    return transcript
--- prompt_diarize_3chunk/split_transcript.py
+++ prompt_diarize_3chunk/split_transcript.py
@@ -8,12 +8,15 @@ from promptflow import tool
 @tool
 def my_python_tool(input1: list, input2: int) -> list:
     out = ''
-
-    for line in input1:
-        out += line['text'] 
     
-    if input2==1: return out[:int(len(out)/3)]
-    if input2==2: return out[int(len(out)/3):2*int(len(out)/3)]
-    if input2==3: return out[2*int(len(out)/3):]
+    if input2==1:
+        for line in input1[:int(len(input1)/3)]:
+            out += line['text']
+    if input2==2: 
+        for line in input1[int(len(input1)/3):2*int(len(input1)/3)]:
+            out += line['text']
+    if input2==3: 
+        for line in input1[2*int(len(input1)/3):]:
+            out += line['text']
 
     return out
